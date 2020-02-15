af239d3e4dee6795bc47e98993988add6ef056e1
EngrBajwa <fixslots@gmail.com>
2020-02-15 08:14:18 -0800

    Week 2 Excercise

---
+++ accounts/src/accounts/Measurable.java
@@ -0,0 +1,5 @@
+package accounts;
+
+public interface Measurable {
+    void getMeasure();
+}
--- src/ParamTest.java
+++ src/ParamTest.java
@@ -1,5 +1,7 @@
 public class ParamTest {
+
     public static void main(String[] args){
         System.out.println("Hello This Is My First Program");
     }
+
 }
